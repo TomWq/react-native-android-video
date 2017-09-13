60b65ef77a1c50fb843405cda78d62095d8e64ea
wangqiang <582752848@qq.com>
2017-09-13 14:47:47 +0800

    修改了

---
+++ index1.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangqiang on 2017/9/13.
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+export default class Indxe extends React.PureComponent{
+    render(){
+        return(
+            <View>
+
+            </View>
+        )
+    }
+}
